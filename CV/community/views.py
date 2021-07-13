47d98be4ec1eea58a5dfb7fe8fed2a8ebff4e518
sa46lll <9812smh@naver.com>
2021-07-13 23:42:07 +0900

    Create Navbar

--- CV/community/views.py
+++ CV/community/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World")
+def index(req):
+    context = {
+
+    }
+    return render(req, "index.html", context=context)
