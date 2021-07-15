dfecf1792313521dd02cacd5da481c8f7d0a9425
juserh <rosapark07@gmail.com>
2021-07-15 20:19:18 +0900

    textarea-datamodel

--- CV/community/models.py
+++ CV/community/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class CV(models.Model):
-    contents = models.TextField()
+#class CV(models.Model):
+#    contents = models.TextField()
+
+class Post(models.Model):
+    job=models.CharField(max_length=50)
+    letter=models.TextField()
+    #strength=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.letter
--- CV/community/views.py
+++ CV/community/views.py
@@ -1,10 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
 
 
 # Create your views here.
-def index(req):
-    context = {
+#def index(req):
+#    context = {
+#
+#    }
+#    return render(req, "index.html", context=context)
 
-    }
-    return render(req, "index.html", context=context)
+def post(request):
+    if request.method=='POST':
+        post=Post()
+        post.letter=request.POST['letter']
+        post.job = request.POST['job']
+        post.save()
+        return redirect('post')
+    else:
+        post=Post.objects.all()
+        return render(request, 'index.html', {'post':post})
